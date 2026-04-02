dc01a0695d646d2be4c849d5093daf59ec861d9b
shubham0419 <shubham.e21@nsut.ac.in>
2026-04-02 12:29:38 +0530

    questions

--- lect5/question2.py
+++ lect5/question2.py
@@ -1,2 +1,21 @@
-# you have given a users array , you have to insert a 
+# you have given a users.json , you have to insert a 
 # product from products.json of id 5 in user cart whose id 5
+import json
+with open("lect5/data/users.json","r") as file:
+  users = json.load(file)
+  user = {}
+  index = 0
+  for x in users:
+    if(x["id"] == 5):
+      user = x
+      index = users.index(x)
+      break
+  with open("lect5/data/products.json","r") as p:
+    products = json.load(p)
+    for x in products:
+      if(x["id"] == 5):
+        user["cart"].append(x)
+        users[index] = user
+        break
+    with open("lect5/data/users.json","w") as u:
+      json.dump(users,u)
